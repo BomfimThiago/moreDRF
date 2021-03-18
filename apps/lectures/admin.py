236868a5ae5d78e47d0f189f9257d2bf85e399df
Thiago Bomfim <thiago.bomfim@smartgreen.net>
2021-03-18 08:16:38 -0300

    final commit

--- apps/lectures/admin.py
+++ apps/lectures/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from apps.lectures.models import Lecture
+
+
 # Register your models here.
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    list_filter = ('title',)
+    search_fields = ('title',)
+    ordering = ('-date',)
+
+
+admin.site.register(Lecture, LectureAdmin)
--- apps/lectures/models.py
+++ apps/lectures/models.py
@@ -5,5 +5,12 @@ from apps.utils.models import TimeStamps
 class Lecture(TimeStamps):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    lecture_name = models.CharField(
+        max_length=100,
+        default='',
+        blank=True,
+        null=True)
+    durations = models.IntegerField(
+        help_text='Enter number of hours',)
     date = models.DateField()
     slides_urls = models.CharField(max_length=255)
